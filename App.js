559c81722d7b28e72f5c339c3d4ebe41b8dcc92e
unknown <braulio.padilla.r@gmail.com>
2024-03-27 18:47:30 +0900

    base code

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Picker,StyleSheet  } from 'react-native';
 
 export default function App() {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleTextInputBlur = () => {
+    console.log('User stopped typing:', text1);
+    // Perform any additional actions here, such as saving the input or triggering events
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        placeholder="write text to translate"
+        onChangeText={text => setText1(text)}
+        value={text1}
+        multiline
+        onBlur={() => handleTextInputBlur()}
+      />
+      <TextInput
+        editable={false}
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        placeholder="Text translated"
+        onChangeText={text => setText2(text)}
+        value={text2}
+        multiline
+      />
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 200 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="English" value="en" />
+        <Picker.Item label="Japanese" value="jp" />
+        <Picker.Item label="Chinese" value="cn" />
+      </Picker>
+      <Text>Selected Value: {selectedValue}</Text>
     </View>
   );
 }
